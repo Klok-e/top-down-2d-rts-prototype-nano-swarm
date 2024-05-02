c8c84db104bfb7160be572e7ddf76d76171ab474
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2024-05-03 00:47:06 +0300

    clippy

--- src/nanobot/debug.rs
+++ src/nanobot/debug.rs
@@ -1,17 +1,15 @@
-use bevy::prelude::{Query, Res, ResMut, Transform, With};
+use bevy::prelude::{Query, Res, Transform, With};
 
 use crate::game_settings::GameSettings;
 
-use super::{components::Nanobot, consts::BOT_RADIUS};
+use super::components::Nanobot;
 
 pub fn bot_debug_circle_system(
     game_settings: Res<GameSettings>,
     bots: Query<&Transform, With<Nanobot>>,
     // mut shapes: ResMut<DebugShapes>,
 ) {
-    if !game_settings.debug_draw_circles {
-        return;
-    }
+    if !game_settings.debug_draw_circles {}
 
     // for transform in bots.iter() {
     //     let translation = transform.translation;
